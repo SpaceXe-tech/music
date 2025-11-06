1edf73d826873e9edba4ced45aea9e6756db7d70
SpaceXe-tech <billaxe299@gmail.com>
2025-11-07 00:31:01 +0530

    Refactor play_logs function for logging structure

--- DeadlineTech/utils/logger.py
+++ DeadlineTech/utils/logger.py
@@ -2,35 +2,42 @@ from pyrogram.enums import ParseMode
 
 from DeadlineTech import app
 from DeadlineTech.utils.database import is_on_off
+from DeadlineTech.misc import SUDOERS
 from config import LOGGER_ID
 
 
 async def play_logs(message, streamtype):
-    if await is_on_off(2):
-        logger_text = f"""
-<b>✨ {app.mention} 𝗉𝗅𝖺𝗒 𝗅𝗈𝗀</b>
-───────────────────────
-
-<b>🆔 𝖢𝗁𝖺𝗍 𝖨𝖣:</b> <code>{message.chat.id}</code>
-<b>🏷️ 𝖢𝗁𝖺𝗍 𝖭𝖺𝗆𝖾:</b> {message.chat.title}
-<b>🔗 𝖢𝗁𝖺𝗍 𝖴𝗌𝖾𝗋𝗇𝖺𝗆𝖾:</b> @{message.chat.username}
-
-<b>👤 𝖴𝗌𝖾𝗋 𝖨𝖣:</b> <code>{message.from_user.id}</code>
-<b>🙋‍♂️ 𝖭𝖺𝗆𝖾:</b> {message.from_user.mention}
-<b>🌐 𝖴𝗌𝖾𝗋𝗇𝖺𝗆𝖾:</b> @{message.from_user.username}
-
-<b>❓ 𝗊𝗎𝖾𝗋𝗒:</b> {message.text.split(None, 1)[1]}
-<b>🎧 𝗌𝗍𝗋𝖾𝖺𝗆 𝗍𝗒𝗉𝖾:</b> {streamtype}
-───────────────────────
-"""
-        if message.chat.id != LOGGER_ID:
-            try:
-                await app.send_message(
-                    chat_id=LOGGER_ID,
-                    text=logger_text,
-                    parse_mode=ParseMode.HTML,
-                    disable_web_page_preview=True,
-                )
-            except:
-                pass
+    if not await is_on_off(2):
         return
+
+    if message.from_user.id in SUDOERS:
+        return
+
+    log_text = f"""
+<b>{app.mention} • Playback trapper</b>
+────────────────────
+
+<b>Chat:</b> {message.chat.title or 'Private Chat'}
+<b>Chat ID:</b> <code>{message.chat.id}</code>
+<b>Username:</b> @{message.chat.username or 'N/A'}
+
+<b>User:</b> {message.from_user.mention}
+<b>User ID:</b> <code>{message.from_user.id}</code>
+<b>Handle:</b> @{message.from_user.username or 'N/A'}
+
+<b>Query:</b> {message.text.split(None, 1)[1]}
+<b>Stream Type:</b> {streamtype}
+────────────────────
+"""
+
+    if message.chat.id != LOGGER_ID:
+        try:
+            await app.send_message(
+                chat_id=LOGGER_ID,
+                text=log_text,
+                parse_mode=ParseMode.HTML,
+                disable_web_page_preview=True,
+            )
+        except:
+            pass
+            
